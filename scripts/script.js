51259ff1c5825f51c8f75bf4a95e3dc80dc32cc3
ZeyarPaing <zeyarpaing@ucsy.edu.mm>
2019-07-22 18:44:21 +0630

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,18 @@
-function testAgain(){
-    
 var ran = (Math.random() * 100) + 1;
 var ran2 = (Math.random() * 100) + 1;
 var average = (ran + ran2) / 2;
 var ava = Math.round(average);
-}
+
 function hello(){
     document.getElementById("overlay").style.display = "block";
     document.getElementById("dialog").style.display = "block";
     var firstPerson = document.getElementById("fp").value;
     var secondPerson = document.getElementById("sp").value;
 
-    if ( ava > 80 ) {
+    if ( out > 80 ) {
         document.getElementById("out").innerHTML = "Love score of " + firstPerson + " and " + secondPerson + " is " + ava + "%."; 
         document.getElementById("add").innerHTML = "The happiest couples never have the same character. They have the best understanding of their differences. ";
-    }else if ( ava > 50 ) {
+    }else if ( out > 50 ) {
         document.getElementById("out").innerHTML = "Love score of " + firstPerson + " and " + secondPerson + " is " + ava + "%."; 
         document.getElementById("add").innerHTML = "The best relationship is when you can act like lovers and best friends at the same time.";
     }else {
@@ -23,9 +21,11 @@ function hello(){
     }
 }
 
-
 function hideDialog() {
     document.getElementById("overlay").style.display = "none";
     document.getElementById("dialog").style.display = "none";
 
+}
+function testAgain() {
+    location.reload();
 }
